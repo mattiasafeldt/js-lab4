ae8851cedd9fd52238eb781a51bce9b8e04d53c0
Mattias Åfeldt <maaf2600@student.miun.se>
2026-09-25 10:13:39 +0200

    Deklarerat variabel för student, kommenterat koden och kört programmet

--- uppgift1.js
+++ uppgift1.js
@@ -3,4 +3,12 @@
 
 let firstName = "Mattias";
 let lastName = "Åfeldt";
+    console.log(firstName + " " + lastName) // Textsträng med förnamn och efternamn
+
 let age = 25;
+    console.log("Ålder:" + " " + age); // Ålder
+
+let isStudent = "true";
+if (isStudent) {
+    console.log("Student:" + " " + isStudent); // Boolean som anger om jag är student eller ej
+}

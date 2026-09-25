166c63e1f946ce774fa4de062e4e930040a1aa42
Mattias Åfeldt <maaf2600@student.miun.se>
2026-09-25 12:47:18 +0200

    Skapat variabel för ålder samt testat koden med olika värden i variabeln

---
+++ uppgift3.js
@@ -0,0 +1,13 @@
+// Lösning till uppgift 3. Av Mattias Åfeldt.
+"use strict";
+
+let age = 20;
+
+if (age < 18) {
+    console.log("Barn");
+} else if (age < 65) {
+    console.log("Vuxen");
+} else {
+    console.log("Pensionär");
+    
+}

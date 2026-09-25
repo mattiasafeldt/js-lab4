a84929126ab2d88c9465727775ff6e9f96e7c425
Mattias Åfeldt <maaf2600@student.miun.se>
2026-09-25 10:44:30 +0200

    Kommenterat kod

--- uppgift2.js
+++ uppgift2.js
@@ -1,7 +1,12 @@
 // Lösning till uppgift 2. Av Mattias Åfeldt.
 "use strict";
 
-let productPrice = 100;
-let quantity = 3;
-let totalPrice = productPrice * quantity;
-let totalPriceVat = totalPrice * 1.25;
+let productPrice = 100; // Pris per produkt
+let quantity = 3; // Antal
+let totalPrice = productPrice * quantity; // Totalpris för samtliga produkter
+let totalPriceVat = totalPrice * 1.25; // Pris inklusive 25% moms
+
+console.log("Pris: " + productPrice + "kr")
+console.log("Antal: " + quantity)
+console.log("Totalt: " + totalPrice)
+console.log("Totalt inklusive moms: " + totalPriceVat + "kr")
